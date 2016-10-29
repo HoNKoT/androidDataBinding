c0ee37f26ac91ca0e6e0e62c3869c465a813a907
Hiroki.H <hon87kot12.xp@gmail.com>
2016-10-29 13:14:44 -0700

    How to get view from id through data binding #2

--- app/src/main/java/honkot/androiddatabinding/sample1/Sample1.java
+++ app/src/main/java/honkot/androiddatabinding/sample1/Sample1.java
@@ -3,6 +3,8 @@ package honkot.androiddatabinding.sample1;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import honkot.androiddatabinding.R;
 import honkot.androiddatabinding.databinding.Sample1Binding;
@@ -18,5 +20,13 @@ public class Sample1 extends AppCompatActivity {
         Sample1Binding binding = DataBindingUtil.setContentView(this, R.layout.sample1);
         User user = new User("Test", "User");
         binding.setUser(user);
+
+        // You can get View Object from ViewID you've defined in layout file.
+        binding.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
