020cea933db738d570dc1890bb7202b314eaac94
Hiroki.H <hon87kot12.xp@gmail.com>
2016-11-09 09:02:50 +0900

    How handle RadioGroup (2) #2

--- app/src/main/java/honkot/androiddatabinding/sample1/Sample5.java
+++ app/src/main/java/honkot/androiddatabinding/sample1/Sample5.java
@@ -1,6 +1,5 @@
 package honkot.androiddatabinding.sample1;
 
-import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,16 +12,16 @@ import honkot.androiddatabinding.databinding.Sample5Binding;
 public class Sample5 extends AppCompatActivity {
 
     private final String TAG = "Sample 1-5";
-    Sample5Binding mBind;
+    UserInput mInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mBind = DataBindingUtil.setContentView(this, R.layout.sample5);
-        UserInput input = new UserInput();
-        mBind.setInput(input);
-        mBind.dump.setOnClickListener(new View.OnClickListener() {
+        Sample5Binding bind = DataBindingUtil.setContentView(this, R.layout.sample5);
+        mInput = new UserInput();
+        bind.setInput(mInput);
+        bind.dump.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dump();
@@ -33,10 +32,10 @@ public class Sample5 extends AppCompatActivity {
     private void dump() {
         StringBuffer buf = new StringBuffer();
         buf.append("RadioGroup:'Radio ");
-        switch (mBind.radioG.getCheckedRadioButtonId()) {
+        switch (mInput.getRadioGroup().get()) {
             case R.id.radioA: buf.append("A"); break;
             case R.id.radioB: buf.append("B"); break;
-            default: buf.append("UNKNOWN:" + mBind.radioG.getCheckedRadioButtonId()); break;
+            default: buf.append("UNKNOWN"); break;
         }
         buf.append("'");
 //        buf.append(System.getProperty("line.separator"));
--- app/src/main/java/honkot/androiddatabinding/sample1/UserInput.java
+++ app/src/main/java/honkot/androiddatabinding/sample1/UserInput.java
@@ -4,20 +4,26 @@ import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.ObservableInt;
 
+import honkot.androiddatabinding.BR;
+import honkot.androiddatabinding.R;
+
 /**
  * Created by hiroki on 2016-10-29.
  */
 public class UserInput extends BaseObservable {
 
     @Bindable
-    private ObservableInt radioGroup;
+    private ObservableInt radioGroup = new ObservableInt();
 
     public UserInput() {
+        // initialize
+        radioGroup.set(R.id.radioA);
     }
     public ObservableInt getRadioGroup() {
         return this.radioGroup;
     }
-    public void setLastName(ObservableInt value) {
+    public void setRadioGroup(ObservableInt value) {
         radioGroup = value;
+        notifyPropertyChanged(BR.radioGroup);
     }
 }
